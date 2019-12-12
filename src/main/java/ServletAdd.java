dbbc95182e6e395b3725e6264948cbe800d4f20c
Ivan <Ivan>
2019-12-12 12:45:17 +0100

    db

--- src/main/java/ServletAdd.java
+++ src/main/java/ServletAdd.java
@@ -1,21 +1,23 @@
+import Classes.User;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-
+import java.util.ArrayList;
 @WebServlet(name = "ServletAdd")
 public class ServletAdd extends javax.servlet.http.HttpServlet {
+//DataSource con;
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws ServletException, IOException {
+
+
 
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws ServletException, IOException {
         PrintWriter pw=response.getWriter();
-        pw.println("<html>");
-        pw.println("<h1><a href=\"https://www.facebook.com/\">click here</a></h1>");
-        pw.println("</html>");
+        DisplayAll ds=new DisplayAll();
+        ArrayList<User> users=ds.ShowUsers();
+        if(users!=null)pw.println(1);else pw.println(users);
     }
 }
